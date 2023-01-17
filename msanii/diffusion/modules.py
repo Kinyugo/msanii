Fix: Issue with timesteps computation

msanii/diffusion/modules.py
@@ -37,7 +37,8 @@ class DiffusionModule(nn.Module):
         total_inference_steps = math.ceil(
             (self.scheduler.num_train_timesteps * num_inference_steps) / (t_start + 1)
         )
-        step = math.ceil((t_start + 1) / num_inference_steps)
+        step = int(((t_start + 1) / num_inference_steps))
+        step = max(step, 1)
         timesteps = (
             torch.arange(start=0, end=int(t_start + 1), step=step, device=device)
             .int()